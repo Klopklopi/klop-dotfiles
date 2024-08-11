7dd96e66cd0e3430003dd3765c6aa46e6f67a746
Hugo JOUBERT <hjoubert@ippon.fr>
2024-08-11 13:48:09 +0200

    feat(dotfiles): added config and install to repo

---
+++ install.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+create_symlinks() {
+	script_dir=$(dirname "$(readlink -f "$0")")
+
+	files=$(find -maxdepth 1 -type f -name ".*")
+
+	for file in $files; do
+		name=$(basename "$file")
+		echo "Creating symlink to $name in home repository"
+		rm -rf ~/"$name"
+		ln -s "$script_dir"/"$name" ~/"$name"
+	done
+
+	if [ ! -d ~/.config/nvim ]; then
+		mkdir -p ~/.config/
+		ln -s "$script_dir"/nvim ~/.config/
+	fi
+
+}
+
+create_symlinks
