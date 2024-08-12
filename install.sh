56634775c3a619e90c2b2e4416a6faadf4be705e
Klopklopi <76015884+Klopklopi@users.noreply.github.com>
2024-08-12 11:45:14 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,13 +21,27 @@ create_symlinks() {
 
 create_symlinks
 
-# Cloner votre repository GitHub (remplacez URL_DE_VOTRE_REPO par l'URL réelle)
-sudo chsh -s /usr/bin/zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#!/bin/bash
 
-# Copier le fichier .zshrc de votre repository vers votre répertoire personnel		
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
-git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
+sudo apt install zsh -y
+# Install oh-my-zsh.
+0>/dev/null sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
+export ZSH_CUSTOM
+# Configure plugins.
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM}"/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions.git "${ZSH_CUSTOM}"/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-history-substring-search "${ZSH_CUSTOM}"/plugins/zsh-history-substring-search
+sudo apt install thefuck autojump -y
+sed -i 's/^plugins=.*/plugins=(git\n gitfast\n terraform\n aws\n git-prompt\n direnv\n git-commit\n extract\n thefuck\n autojump\n jsontools\n colored-man-pages\n zsh-autosuggestions\n zsh-syntax-highlighting\n zsh-history-substring-search\n you-should-use\n debian)/g' ~/.zshrc
+# Install powerlevel10k and configure it.
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM}"/themes/powerlevel10k
+sed -i 's/^ZSH_THEME=.*/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' ~/.zshrc
+# Move ".zcompdump-*" file to "$ZSH/cache" directory.
+sed -i -e '/source \$ZSH\/oh-my-zsh.sh/i export ZSH_COMPDUMP=\$ZSH\/cache\/.zcompdump-\$HOST' ~/.zshrc
+# Configure the default ZSH configuration for new users.
+sudo cp ~/.zshrc /etc/skel/
+sudo cp ~/.p10k.zsh /etc/skel/
+sudo cp -r ~/.oh-my-zsh /etc/skel/
+sudo chmod -R 755 /etc/skel/
+sudo chown -R root:root /etc/skel/
